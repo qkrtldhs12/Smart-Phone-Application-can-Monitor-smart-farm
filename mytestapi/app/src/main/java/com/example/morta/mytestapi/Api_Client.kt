363a1ddcd1952693b215727b7c33bcd29c6bd6d2
a1216843 <49019204+a1216843@users.noreply.github.com>
2021-04-01 04:09:36 +0900

    API호출 ver.Neople

--- mytestapi/app/src/main/java/com/example/morta/mytestapi/Api_Client.kt
+++ mytestapi/app/src/main/java/com/example/morta/mytestapi/Api_Client.kt
@@ -3,11 +3,16 @@ package com.example.morta.mytestapi
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
-interface GithubClient {
+interface Api_Client {
     //@GET 어노테이션으로 GET Request를 사용
-    @GET("/users/{user}/repos")
+    //@GET("/users/{user}/repos")
+    @GET("df/servers/all/characters")
     //@Path()어노테이션으로 url에 들어갈 {user}를 동적으로 만들 수 있다.
-    fun reposForUser(@Path("user")user:String): Call<List<GitHubRepo>>
+    fun query(
+            @Query("characterName") characterName: String,
+            @Query("apikey") apikey: String
+    ): Call<NeopleRepo>
     //Call<>object를 사용해서 반환값을 비동기식으로 구현
 }
--- mytestapi/app/src/main/java/com/example/morta/mytestapi/GitHubRepo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.morta.mytestapi
-
-import com.google.gson.annotations.SerializedName
-
-data class GitHubRepo (@SerializedName("name") val name: String)
