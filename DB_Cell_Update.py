76b4cdb01c14f59ce6aa40f97cf0e446dd498555
klustar <49019229+klustar@users.noreply.github.com>
2021-10-09 01:07:27 +0900

    Add files via upload

--- DB_Cell_Update.py
+++ DB_Cell_Update.py
@@ -1,55 +1,146 @@
+import threading
+from time import sleep
+import time
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime
 
-cred = credentials.Certificate("spam-85498-firebase-adminsdk-wsavj-a18faa0fe2.json")
+class Device(object):
+    def __init__(self, source):
+        self.model_id = source.get("model_id")
+        self.name = source.get("name")
+        self.connected = source.get("connected")
+        self.door = source.get("door")
+        self.light = source.get("light")
+        self.heat = source.get("heat")
+        self.humidifier = source.get("humidifier")
+        self.vent = source.get("vent")
+
+    def from_dict(self, source):
+        #dict로부터 Device로 변환
+        self.model_id = source.get("model_id")
+        self.name = source.get("name")
+        self.connected = source.get("connected")
+        self.door = source.get("door")
+        self.light = source.get("light")
+        self.heat = source.get("heat")
+        self.humidifier = source.get("humidifier")
+        self.vent = source.get("vent")
+
+    def to_dict(self):
+        #Device로부터 dict로 변환
+        new_dict = {
+            "model_id" : self.model_id,
+            "name" : self.name,
+            "connected" : self.connected,
+            "door" : self.door,
+            "light" : self.light,
+            "heat" : self.heat,
+            "humidifier" : self.humidifier,
+            "vent" : self.vent
+        }
+        return new_dict
+
+class Cell(object):
+    def __init__(self, source):
+        self.model_id = source.get("model_id")
+        self.name = source.get("name")
+        self.humi = source.get("humi")
+        self.soil = source.get("soil")
+        self.temp = source.get("temp")
+        self.viewtype = 1
+
+    def from_dict(self, source):
+        self.model_id = source.get("model_id")
+        self.name = source.get("name")
+        self.humi = source.get("humi")
+        self.soil = source.get("soil")
+        self.temp = source.get("temp")
+        self.viewtype = 1
+
+    def to_dict(self):
+        new_dict = {
+            "model_id": self.model_id,
+            "name": self.name,
+            "humi": self.humi,
+            "soil": self.soil,
+            "temp": self.temp,
+            "viewtype": int(1)
+        }
+        return new_dict
+
+    def diff_check(self, source):
+        # dict형태로 데이터 받아서 현재 Cell 데이터에 변화가 없다면 True 반환
+        if self.humi != source.get("humi"):
+            return False
+        elif self.soil != source.get("soil"):
+            return False
+        elif self.temp != source.get("temp"):
+            return False
+        else:
+            return True
+
+cred = credentials.Certificate("d:/git/SPAM_SF/spam-85498-firebase-adminsdk-wsavj-7375d295d7.json")
 # 파이어베이스 연결용 비공개 키
 firebase_admin.initialize_app(cred)
 db = firestore.client() # Firestore 객체 생성
-Device_id = "12345" # 각 기기별 고유번호, User_info 밑에 저장될 데이터
+model_id = "default" # 모델명(고유값)
+user_email = "caesar2746@gmail.com"
 
-doc_ref = db.collection('Device_data').document(Device_id)
-# Firestore에서 Device_data 컬렉션 내부에 Device_id를 이름으로 현재 데이터값 갱신
-doc_ref.set({
-    'cell' : {
-        'cell_1' : {
-            'Soil' : 4,
-            'Lumi' : 4,
-            'Temp' : 4,
-            'Humi' : 4
-        },
-        'cell_2' : {
-            'Soil' : 4,
-            'Lumi' : 3,
-            'Temp' : 2,
-            'Humi' : 1
-        }
+# 해당하는 모델명을 등록한 유저의 이메일 조회
+doc_ref = db.collection("User_Email").document("default")
+doc = doc_ref.get()
+if doc.exists:
+    user_email = doc.get("Email")
+
+
+# 조회한 유저 이메일로 디바이스 정보 조회 -> 디바이스 실행시 한 번 수행한 뒤엔 제어신호 들어올 때만 조회 및 갱신
+doc_ref = db.collection("User_info").document(user_email).collection("User_Device").document(model_id)
+device = Device(doc_ref.get())
+
+# 디바이스에 연결된 모든 셀 정보 조회
+doc_ref = db.collection("User_info").document(user_email).collection("User_Device").document(model_id).collection("Cell_Data").stream()
+All_Cell = [] # 디바이스 내의 모든 Cell 정보를 저장하는 리스트
+for doc in doc_ref:
+    cell = Cell(doc)
+    All_Cell.append(cell)
+
+callback_done = threading.Event()
+
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        # User_info ~ Received 경로에서 Received에 변화가 있을 경우 아래 로직이 실행됨
+        # 앱에서 제어신호를 Received로 보내면 이쪽에서 처리할 것
+        pass
+    callback_done.set()
+
+doc_ref = db.collection("User_info").document(user_email).collection("User_Device").document(model_id).collection("Control").document("Received")
+
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+# DB에 접근하는 횟수 감소를 위해서 일정 주기가 아닌 센서값에 변화가 생길 때만 DB에 갱신
+while(1):
+    # received_data는 아두이노로부터 받은 데이터 예시
+    received_data = {
+        "name": "test",
+        "model_id": "test",
+        "humi": 68,
+        "soil": 14,
+        "temp": 21,
+        "viewtype": 1
     }
-    # TODO: 센서 측정값, 제어시 사용할 변수값 JSON 형태로 DB 스키마 참고해서 추가할 것
-})
-
-#시간별 데이터 누적용, 누적 데이터는 Device_id 하위 컬렉션 Data_log에 현재 시간 기준으로 기록
-time = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-doc2_ref = db.collection('Device_data').document(Device_id).collection("Data_log").document(time)
-doc2_ref.set({
-    'cell' : {
-        'cell_1' : {
-            'Soil' : 4,
-            'Lumi' : 4,
-            'Temp' : 4,
-            'Humi' : 4
-        },
-        'cell_2' : {
-            'Soil' : 4,
-            'Lumi' : 3,
-            'Temp' : 2,
-            'Humi' : 1
-        }
-    }})
-
-try:
-    doc = doc_ref.get()
-    print("Document data :".format(doc.to_dict()))
-except:
-    print("No such doc")
+    # 현재 가지고 있는 모든 셀 정보(All_Cell)와 수신한 셀 정보(received_data)를 비교
+    for cell in All_Cell:
+        # All_Cell에서 name을 기준으로 received_data와 일치하는 셀 검색
+        if(received_data.get("name") == cell.to_dict().get("name")):
+            # 기존의 셀 정보와 수신한 셀 정보가 동일한지 검사 -> False면 불일치, True면 일치
+            if(cell.diff_check(received_data) == False):
+                # 셀 정보 업데이트
+                cell_update = db.collection("User_info").document(user_email).collection("User_Device").document(model_id).collection("Cell_Data").document(received_data.get("name"))
+                cell_update.set(received_data)
+                All_Cell[cell]=Cell(received_data)
+    
+    print('1')
+    time.sleep(5)
--- conAru.py
+++ conAru.py
@@ -1,5 +1,9 @@
 from serial import Serial
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
 
 '''
 # 센싱아두이노 시리얼포트 연결-라즈베리파이
@@ -7,26 +11,30 @@ ser1=Serial('/dev/ttyACM0',9600)
 ser2=Serial('/dev/ttyACM1',9600)
 '''
 # 센싱아두이노 시리얼포트 연결-윈도우10
-ser1=Serial('COM10',9600)    #sen
-#ser2=Serial('COM10',9600)   #con
+ser1=Serial('COM7',9600)    #sen
+ser2=Serial('COM10',9600)   #con
 
 def conS(s,cel,con):
     '''
     토양수분센서 데이터를 받아 수분공급하는 코드
     코드표기방식
-    토양수분공급 on/off : 1/2
+    토양수분공급 on/off : c/d
     ''' 
     
     level=25    # 식물마다 정해진 적정수분량
                 # 원래는 스마트팜API에서 받아온 변수로 설정해야함.
     if s<level:
-        con.write(b'\x1f')
+        code='c'
+        code=code.encode('utf-8')
+        con.write(code)
     else:
-        con.write(b'\x2f')
+        code='d'
+        code=code.encode('utf-8')
+        con.write(code)
     
     return
 
-while ser1.readable()!='\0':
+while (1):
     if ser1.readable():
         res1=ser1.readline()
         res1=str(res1)
@@ -34,7 +42,7 @@ while ser1.readable()!='\0':
         ref1[0]=ref1[0][-1]
         ref1[3]=ref1[3][:-6]
 
-        print(ref1) 
-        #conS(int(ref1[1]),'1',ser2)                                            
+        conS(int(ref1[1]),'1',ser2)
+        print(ref1)
 
     time.sleep(5)
